82dcce6bc224c74779ac4fcab00aa08bfab3b59f
ArtemReva <artemreva79@gmail.com>
2025-03-15 20:35:43 +0300

    chore: bump version to 0.2.2 and add Sentry for error tracking

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,12 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+
+import * as Sentry from "@sentry/react";
+
+// Initialize Sentry
+Sentry.init({
+  dsn: "https://72cabbbdafe87f51a39927bec3d9e076@o4508982929588224.ingest.de.sentry.io/4508982935617616",
+});
 
 createRoot(document.getElementById("root")!).render(<App />);
